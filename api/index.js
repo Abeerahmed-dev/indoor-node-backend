57a4440fa202810d37bebf5372c8d630a865b3f9
Abeerahmed-dev <abeertanveer806@gmail.com>
2026-05-20 23:38:15 +0500

    CORs allowed

--- api/index.js
+++ api/index.js
@@ -3,7 +3,11 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json());
 
 app.get("/", (req, res) => {
