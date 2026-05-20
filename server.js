0ca97c8a380fbf99536c866e4b0ce5f8c32a9283
Abeerahmed-dev <abeertanveer806@gmail.com>
2026-05-21 00:48:59 +0500

    updated server js file for Render

--- server.js
+++ server.js
@@ -20,10 +20,14 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
+app.use(express.urlencoded({ extended: true }));
 // Routes
 app.use('/api/courts', courtRoutes);
 app.use('/api/users', userRoutes);
